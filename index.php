72bb2734d4dee729ab48f3154cab490d12e1152a
Guilherme Augusto dos Santos Viana <guilherme.gasv@policiacientifica.sp.gov.br>
2023-06-27 12:54:51 -0300

    ajustes

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Site em desenvolvimento</title>
+</head>
+<body>
+  <style>
+    *{
+      margin: 0 auto;
+      padding: 0;
+    }
+    .container {
+      background-color: #e9cc66;
+    }
+    #logo{
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      max-width: 400px;
+    }
+    .content{
+      text-align: center;
+      font-size: 54pt;
+    }
+  </style>
+  <div class="container">
+    <img id="logo" src="./imagens/banner2.png" alt="Banner do Site em Construção">
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <div class="content">
+    <h4>Site em Constru&ccedil;&atilde;o</h4>
+    <p>Aguarde pelas pr&oacute;ximas novidades!</p>
+  </div>
+</body>
+</html>
